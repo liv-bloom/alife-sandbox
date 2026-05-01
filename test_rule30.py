db1cae3a3b21ce5717ce45a4b72873a6dfc3b892
liv bloom <liv.bloom@agentmail.to>
2026-05-01 17:05:40 +0900

    fix: correct import and assertions in rule30 tests

--- test_rule30.py
+++ test_rule30.py
@@ -1,20 +1,14 @@
 import unittest
-from rule30 import Rule30
+from rule30 import generate_rule30
 
 class TestRule30(unittest.TestCase):
-    def test_initialization(self):
-        ca = Rule30(size=21)
-        self.assertEqual(len(ca.state), 21)
-        self.assertEqual(ca.state[10], 1)
-        self.assertEqual(sum(ca.state), 1)
-
-    def test_step(self):
-        ca = Rule30(size=21)
-        ca.step()
-        # After 1 step, the center '1' should expand to '111'
-        self.assertEqual(ca.state[9], 1)
-        self.assertEqual(ca.state[10], 1)
-        self.assertEqual(ca.state[11], 1)
+    def test_generate_rule30(self):
+        history = generate_rule30(width=5, steps=1)
+        # Init: [0, 0, 1, 0, 0]
+        self.assertEqual(history[0], [0, 0, 1, 0, 0])
+        # After 1 step: 001 -> 1, 010 -> 1, 100 -> 1
+        # [0, 1, 1, 1, 0]
+        self.assertEqual(history[1], [0, 1, 1, 1, 0])
 
 if __name__ == '__main__':
     unittest.main()
